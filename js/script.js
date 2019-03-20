42d275d2c954ecd23c8190f32013c53088a5cb41
Alexander Cai <alexandercai@outlook.com>
2019-03-20 13:06:33 -0600

    broken

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Author: Michael Hadley, mikewesthad.com
+ * Asset Credits:
+ *  - Tuxemon, https://github.com/Tuxemon/Tuxemon
+ */
+
+import MainScene from './scenes/mainScene';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "game-container",
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  },
+  scene: MainScene
+};
+
+const game = new Phaser.Game(config);
